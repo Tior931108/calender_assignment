8de1b17f8c471b676efb18194a787cf9430d4a3b
Tior931108 <jyj8469@gmail.com>
2025-11-03 22:04:14 +0900

    ✨ feat: [필수 lv.1] main 브랜치 변경 및 DB 연결 초기 설정

--- src/main/java/com/example/calender/CalenderApplication.java
+++ src/main/java/com/example/calender/CalenderApplication.java
@@ -2,7 +2,9 @@ package com.example.calender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CalenderApplication {
 
