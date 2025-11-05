fdbcc0d056c9b41f475512523903eceffa816265
Tior931108 <jyj8469@gmail.com>
2025-11-05 16:21:18 +0900

    ♻ refactor: [도전 lv.6] 일정 및 댓글 Controller Layer에 ResponseEntity 적용
    - 정확한 상태 코드 표시
    - 수정 매핑 PUT > PATCH 변경

--- src/main/java/com/example/calender/controller/CalenderController.java
+++ src/main/java/com/example/calender/controller/CalenderController.java
@@ -3,6 +3,8 @@ package com.example.calender.controller;
 import com.example.calender.dto.*;
 import com.example.calender.service.CalenderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,33 +15,41 @@ public class CalenderController {
 
     private final CalenderService calenderService;
 
+    // ResponseEntity.status(HttpStatus.상태).body(응답데이터);
+    // ResponseEntity : HTTP 상태코드를 정확히 전달하기 위해 사용
+    // 아무런 상태값도 정해주지 않는다면 기본적으로 Spring은 HTTP 상태 코드 '200'으로 전달함.
+
     // 저장
     @PostMapping("/calenders")
-    public CreateCalenderResponse createCalender(
+    public ResponseEntity<CreateCalenderResponse> createCalender(
             @RequestBody CreateCalenderRequest createCalenderRequest) {
-        return calenderService.save(createCalenderRequest);
+        CreateCalenderResponse result = calenderService.save(createCalenderRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
     // 전체 조회 (작성자명 필터링 가능)
     @GetMapping("/calenders")
-    public List<GetUserNameCalenderResponse> getAllCalenders(
+    public ResponseEntity<List<GetUserNameCalenderResponse>> getAllCalenders(
             @RequestParam(required = false) String userName) {
-        return calenderService.findAll(userName);
+        List<GetUserNameCalenderResponse> results = calenderService.findAll(userName);
+        return ResponseEntity.status(HttpStatus.OK).body(results);
     }
 
-    // 선택 일정 조회 (단 건 조회)
+    // 선택 일정 조회 (단 건 조회) - 댓글 내역 포함
     @GetMapping("/calenders/{id}")
-    public GetOneCalenderResponse getOneCalender(@PathVariable Long id) {
-        return calenderService.getOneCalender(id);
+    public ResponseEntity<GetOneCalenderResponse> getOneCalender(@PathVariable Long id) {
+        GetOneCalenderResponse result = calenderService.getOneCalender(id);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     // 수정
-    @PutMapping("/calenders/{id}")
-    public UpdateCalenderResponse updateCalender(
+    @PatchMapping("/calenders/{id}")
+    public ResponseEntity<UpdateCalenderResponse> updateCalender(
             @PathVariable Long id,
             @RequestBody UpdateCalenderRequest updateCalenderRequest
     ) {
-        return calenderService.updateCalender(id, updateCalenderRequest);
+        UpdateCalenderResponse result = calenderService.updateCalender(id, updateCalenderRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
 
@@ -47,10 +57,11 @@ public class CalenderController {
     // @RequestParam 사용시, URL에 비밀번호 노출 가능성 증가
     // 보안성을 위해서 @RequestBody 사용
     @DeleteMapping("/calenders/{id}")
-    public void deleteCalender(
+    public ResponseEntity<Void> deleteCalender(
             @PathVariable Long id,
             @RequestBody DeleteCalPwdRequest deleteCalPwdRequest
     ) {
         calenderService.deleteCalender(id, deleteCalPwdRequest);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/example/calender/controller/CommentController.java
+++ src/main/java/com/example/calender/controller/CommentController.java
@@ -4,6 +4,8 @@ import com.example.calender.dto.CommentResponse;
 import com.example.calender.dto.CreateCommentRequest;
 import com.example.calender.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,16 +18,18 @@ public class CommentController {
 
     // 댓글 생성
     @PostMapping("/calenders/{calId}/comments")
-    public CommentResponse createComment(
+    public ResponseEntity<CommentResponse> createComment(
             @PathVariable Long calId,
             @RequestBody CreateCommentRequest createCommentRequest) {
-        return commentService.createComment(calId, createCommentRequest);
+        CommentResponse result = commentService.createComment(calId, createCommentRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
     // 특정 일정의 댓글 전체 조회
     @GetMapping("/calenders/{calId}/comments")
-    public List<CommentResponse> getCommentsByCalenderId(
+    public ResponseEntity<List<CommentResponse>> getCommentsByCalenderId(
             @PathVariable Long calId) {
-        return commentService.getCommentsByCalenderId(calId);
+        List<CommentResponse> results = commentService.getCommentsByCalenderId(calId);
+        return ResponseEntity.status(HttpStatus.OK).body(results);
     }
 }
